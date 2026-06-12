5d383b4b4d83cdf9c638745036250296722b4cb5
argroy <royarghyadip6@gmail.com>
2026-06-12 14:36:51 +0530

    E-Commerce: Uploading Config Client MS

---
+++ config/src/main/java/com/appconfiguration/config/BuildInfo.java
@@ -0,0 +1,14 @@
+package com.appconfiguration.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "build")
+public class BuildInfo {
+    private String id;
+    private String name;
+    private String version;
+}
---
+++ config/src/main/java/com/appconfiguration/config/controller/BuildInfoController.java
@@ -0,0 +1,26 @@
+package com.appconfiguration.config.controller;
+
+import com.appconfiguration.config.BuildInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BuildInfoController {
+
+ /*   @Value("${build.id:default-id}")
+    private String buildId;
+    @Value("${build.name:default-name}")
+    private String buildName;
+    @Value("${build.version:default-version}")
+    private String buildVersion;*/
+
+    private final BuildInfo buildInfo;
+
+    @GetMapping("/build-info")
+    public String getBuildInfo() {
+        return "Build ID: " + buildInfo.getId() + ", Build Name: " + buildInfo.getName() + ", Build Version: " + buildInfo.getVersion();
+    }
+}
