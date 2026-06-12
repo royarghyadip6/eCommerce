7db9d4c03d20fbf5400303c3b4bc98d3bdd2356a
argroy <royarghyadip6@gmail.com>
2026-06-12 19:21:46 +0530

    E-Commerce: Load config data without refresh and Zero downtime using Actuator

--- config/src/main/java/com/appconfiguration/config/controller/BuildInfoController.java
+++ config/src/main/java/com/appconfiguration/config/controller/BuildInfoController.java
@@ -3,25 +3,34 @@ package com.appconfiguration.config.controller;
 import com.appconfiguration.config.BuildInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RefreshScope
 public class BuildInfoController {
 
- /*   @Value("${build.id:default-id}")
+    @Value("${build.id:default-id}")
     private String buildId;
+    @Value("${build.id:default-owner}")
+    private String buildOwner;
     @Value("${build.name:default-name}")
     private String buildName;
     @Value("${build.version:default-version}")
-    private String buildVersion;*/
+    private String buildVersion;
 
     private final BuildInfo buildInfo;
 
+    /**
+     * Configuration will automatically refresh once <a href="http://localhost:8080/actuator/refresh">http://localhost:8080/actuator/refresh</a> is called, no need to restart the application
+     * @return
+     */
     @GetMapping("/build-info")
     public String getBuildInfo() {
 //        return "Build ID: " + buildInfo.getId() + ",Build Owner: " + buildInfo.getOwner() + ", Build Name: " + buildInfo.getName() + ", Build Version: " + buildInfo.getVersion();
-        return buildInfo.toString();
+        return "Build ID: " + buildId + ",Build Owner: " + buildOwner + ", Build Name: " + buildName + ", Build Version: " + buildVersion;
     }
+
 }
