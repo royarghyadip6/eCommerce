febd33a8af608d999f98508b356449aa749ce37b
argroy <royarghyadip6@gmail.com>
2026-06-14 17:13:17 +0530

    E-Commerce: Adding Configuration Server for microservices

---
+++ EComConfigServer/src/test/java/com/EComConfigServer/EComConfigServer/EComConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EComConfigServer.EComConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
